691d2ee2cebe5d0b8f34244f6b568110f6157170
Rado M <radkomih@gmail.com>
2024-07-22 10:04:42 +0300

    nit

--- core/types/preconf_receipt.go
+++ core/types/preconf_receipt.go
@@ -4,11 +4,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// Pre-confirmation receipts are similar to normal receipts, but
-// they also contain additional fields that are not expected to
-// be resolved later on and the storage is also different.
-
-// PreconfReceipt extends Receipt by including From and To fields.
+// PreconfReceipt extends Receipt by including additional From and To fields.
 type PreconfReceipt struct {
 	Receipt
 	From common.Address
