c4bf81c8fe25ea241f7a8d20126b2da410419197
failfmi <oscurocalma@gmail.com>
2024-08-01 12:44:41 +0300

    scripts(preconf): modify txs

--- test-preconf/main.go
+++ test-preconf/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"crypto/ecdsa"
+	"errors"
 	"fmt"
+	"github.com/ethereum/go-ethereum"
 	"log"
 	"math/big"
 
@@ -24,56 +26,17 @@ var (
 		{
 			"privateKey":  "bcdf20249abf0ed6d944c0288fad489e33f66b3960d9e6229c1cd214ed3bbe31",
 			"fromAddress": "0x8943545177806ED17B9F23F0a21ee5948eCaa776",
-			"toAddress":   "0x0000777735367b36bC9B61C50022d9D0700dB4Ec",
+			"toAddress":   "0xE25583099BA105D9ec0A67f5Ae86D90e50036425",
 		},
-		{ // golden touch account
-			"privateKey":  "92954368afd3caa1f3ce3ead0069c1af414054aefe1ef9aeacc1bf426222ce38",
-			"fromAddress": "0x0000777735367b36bC9B61C50022d9D0700dB4Ec",
+		{
+			"privateKey":  "39725efee3fb28614de3bacaffe4cc4bd8c436257e2c8bb887c4b5c4be45e76d",
+			"fromAddress": "0xE25583099BA105D9ec0A67f5Ae86D90e50036425",
 			"toAddress":   "0x8943545177806ED17B9F23F0a21ee5948eCaa776",
 		},
 	}
 
-	nonce = uint64(1718)
-
-	accountsTxs = map[string][]interface{}{
-		"0x8943545177806ED17B9F23F0a21ee5948eCaa776": {
-			types.DynamicFeeTx{Nonce: nonce + 0},
-			types.DynamicFeeTx{Nonce: nonce + 1},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 2, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 3, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
-
-			types.DynamicFeeTx{Nonce: nonce + 2},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 2, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 3, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(6))},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 4, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(7))},
-			types.DynamicFeeTx{Nonce: nonce + 3},
-			types.DynamicFeeTx{Nonce: nonce + 4},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 5, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(8))},
-
-			types.DynamicFeeTx{Nonce: nonce + 6},
-			types.DynamicFeeTx{Nonce: nonce + 7},
-		},
-		"0x0000777735367b36bC9B61C50022d9D0700dB4Ec": {
-			types.DynamicFeeTx{Nonce: nonce + 0},
-			types.DynamicFeeTx{Nonce: nonce + 1},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 2, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 3, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
-
-			types.DynamicFeeTx{Nonce: nonce + 2},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 2, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 3, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(6))},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 4, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(7))},
-			types.DynamicFeeTx{Nonce: nonce + 3},
-			types.DynamicFeeTx{Nonce: nonce + 4},
-			types.InclusionPreconfirmationTx{Nonce: nonce + 5, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(8))},
-
-			types.DynamicFeeTx{Nonce: nonce + 6},
-			types.DynamicFeeTx{Nonce: nonce + 7},
-		},
-	}
-
-	value    = big.NewInt(1000000000000000000) // in wei (1 eth)
-	gasLimit = uint64(200000)                  // in units
+	value    = big.NewInt(100000) //
+	gasLimit = uint64(200000)     // in units
 	data     []byte
 )
 
@@ -101,12 +64,41 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to get nonce: %v", err)
 		}
-		fmt.Println("Nonce:", nonce)
+		fmt.Println()
+		fmt.Println("Account:", fromAddress)
+
+		accountsTxs := map[string][]interface{}{
+			"0x8943545177806ED17B9F23F0a21ee5948eCaa776": {
+				types.InclusionPreconfirmationTx{Nonce: nonce, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 1, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 2, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 3, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 4, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 5, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.DynamicFeeTx{Nonce: nonce + 6}, // does not have immediate receipt
+				types.InclusionPreconfirmationTx{Nonce: nonce + 7, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(-1))},  // past deadline
+				types.InclusionPreconfirmationTx{Nonce: nonce + 8, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(1))},   // not assigned slot
+				types.InclusionPreconfirmationTx{Nonce: nonce + 9, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(100))}, // not assigned slot, in next L1 epoch
+			},
+			"0xE25583099BA105D9ec0A67f5Ae86D90e50036425": {
+				types.InclusionPreconfirmationTx{Nonce: nonce, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 1, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 2, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 3, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.InclusionPreconfirmationTx{Nonce: nonce + 4, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(5))},
+				types.DynamicFeeTx{Nonce: nonce + 5}, // does not have immediate receipt
+				types.InclusionPreconfirmationTx{Nonce: nonce + 6, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(-1))},  // past deadline
+				types.InclusionPreconfirmationTx{Nonce: nonce + 7, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(1))},   // not assigned slot
+				types.InclusionPreconfirmationTx{Nonce: nonce + 8, Deadline: big.NewInt(0).Add(currentSlot, big.NewInt(100))}, // not assigned slot, in next L1 epoch
+			},
+		}
 
 		// Send pre-configured number of txs
 		accountTxs := accountsTxs[account["fromAddress"]]
 
 		for i := 0; i < len(accountTxs); i++ {
+			fmt.Println()
+
 			// nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 			// if err != nil {
 			// 	log.Fatalf("Failed to get nonce: %v", err)
@@ -148,8 +140,29 @@ func main() {
 				log.Fatalf("Failed to send transaction: %v", err)
 			}
 
-			fmt.Printf("Tx hash: %s\n", signedTx.Hash().Hex())
-		}
+			fmt.Printf("Submitted Tx hash: %s\n", signedTx.Hash().Hex())
 
+			_, _, err = client.TransactionByHash(context.Background(), signedTx.Hash())
+			if err != nil {
+				if errors.Is(err, ethereum.NotFound) {
+					fmt.Println(fmt.Sprintf("Tx Hash [%s]: Transaction not found", signedTx.Hash()))
+					continue
+				} else {
+					log.Fatalf("Failed to get transaction by hash %v, hash %s", err, signedTx.Hash())
+				}
+			}
+
+			txReceipt, err := client.TransactionReceipt(context.Background(), signedTx.Hash())
+			if err != nil {
+				if errors.Is(err, ethereum.NotFound) {
+					fmt.Println(fmt.Sprintf("Tx Hash [%s]: Transaction receipt not found", signedTx.Hash()))
+					continue
+				} else {
+					log.Fatalf("Failed to get transaction receipt: %v", err)
+				}
+			}
+
+			fmt.Println(fmt.Sprintf("TxHash: [%s], Transaction receipt, Status: [%d], Block Number: [%d]", signedTx.Hash(), txReceipt.Status, txReceipt.BlockNumber))
+		}
 	}
 }
